097da49fc67d25f0983616482fbfb47f04c34224
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-05-31 21:37:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ APP_ID = int(os.environ.get("APP_ID", "1040456"))
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "ed1edeaedb17b440dbb29b91ae757270")
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002023444138"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002230365222"))
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1480923991"))
 #Port
